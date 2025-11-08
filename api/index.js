d24bc8fa9dc4421032f594a8db40e1abe3bc6153
elijah-ug <elijahtalentmugisha14@gmail.com>
2025-11-09 01:40:59 +0300

    migrated cart calculations to the backend

--- api/index.js
+++ api/index.js
@@ -9,7 +9,8 @@ import cartRoute from "./src/routes/cartRoutes.js";
 dotenv.config();
 const app = express();
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://e-com-fst8.onrender.com"],
+  origin: ["http://localhost:5173", "https://shopping-cart-mauve-theta.vercel.app"],
+
   methods: ["GET", "POST", "PUT", "DELETE"],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   allowedHeaders: ["Content-Type", "Authorization"],
