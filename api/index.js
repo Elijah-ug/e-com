bcc85f7bb9da191db51f54cff9f4416dad6d8e68
elijah-ug <elijahtalentmugisha14@gmail.com>
2025-11-18 02:13:33 +0300

    added pagintion on products and movined search item functionality to nav + other changes

--- api/index.js
+++ api/index.js
@@ -9,12 +9,7 @@ import cartRoute from "./src/routes/cartRoutes.js";
 dotenv.config();
 const app = express();
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://shopping-cart-mauve-theta.vercel.app/",
-    "https://e-com-fst8.onrender.com/",
-  ],
-
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   allowedHeaders: ["Content-Type", "Authorization"],
